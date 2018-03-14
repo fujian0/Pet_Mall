207e9e0a05b883b94c8e3847123c307aa6bf568c
fujian0 <fujian0315@sina.com>
2018-03-15 00:08:40 +0800

    项目说明更新

--- README.md
+++ README.md
@@ -1,17 +1,53 @@
-2018/3/14项目第一天(我的E宠)
-### 项目总结
-  ##使用到的工具
-    1.使用vue-cli(脚手架)搭建项目
-    2.使用vue-router配置路由器
-    3.使用iconfont字体图标 - 阿里巴巴矢量库（http://www.iconfont.cn/）
-    4.(使用stylus预编译器)stylus中文版参考文档(http://www.zhangxinxu.com/jq/stylus/)
+2018/3/14项目day01(我的E宠)
+# 项目总结 
 
-  ##项目已完成功能
-    底部路由切换功能
-    头部搜索功能
+### 项目目录结构 ###
 
-  ##遇到的困难
-    路由配置路径不太清楚(已解决)
+### 1.项目目录
+
+
+	├─ build/            # Webpack 配置目录
+	├─ config/           # Webpack 配置目录
+	├─ dist/			 # build 生成的生产环境下的项目
+	├─ src/              # 源码目录（开发都在这里进行）
+	│   ├─ api/ 		   # 发送ajax请求，返回响应
+	│   ├─ common/         # 放置需要经由 Webpack 处理的静态文件
+	│   ├─ components/     # 组件（COMPONENT）
+	│   ├─ mock/		   # 模拟请求返回的数据
+	│   ├─ pages/          # 路由组件目录
+	│   │   ├─ classify/      # 分类路由组件
+	│   │   ├─ firstPage/     # 首页路由组件
+	│   │   ├─ shopping/      # 购物车路由组件
+	│   │   ├─ userCenter/    # 登录/注册/用户中心路由组件
+	│   ├── router/        # 路由配置（ROUTE）
+	│   ├── store/         # vuex状态管理目录
+	│   ├── App.vue        # 初始化页面
+	│   ├── main.js        # 启动文件
+	├── static/          # 放置无需经由 Webpack 处理的静态文件
+	├── .babelrc         # Babel 转码配置
+	├── .eslintignore    # （配置）ESLint 检查中需忽略的文件（夹）
+	├── .eslintrc        # ESLint 配置
+	├── .gitignore       # （配置）需被 Git 忽略的文件（夹）
+	├── package.json     # （依赖包）
+  
+###2. 技术选型
+### vue全家桶
+    * "vue""
+    * vue-cli    (脚手架)搭建项目
+    * "vue-lazyload"  （懒加载）
+    * "vue-router"   (路由器)
+    * "vue-scroller"（滑屏库）
+    * "vuex"    （状态管理模式）
+    * "iconfont"  字体图标 - 阿里巴巴矢量库（http://www.iconfont.cn/）
+    * "stylus"    预编译器 stylus中文版参考文档(http://www.zhangxinxu.com/jq/stylus/)
+    
+   
+
+#####* 项目已完成功能
+    * 底部路由切换功能
+    * 头部搜索功能
+
+ 
 
 
 
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,27 +1,80 @@
 <template>
-  <div class="mind">
-    <div class="clearfix pt5 pl10 pr10 pb5">
-      <div class="epet-search bgf">
-        <div class="fl rela ft14 location">
-          <a :href="EpetJs.Common.createUrl('place','?pet_type='+pet_type)">
-            <span class="catordog c89">狗狗</span>
-            <span class="c89">|</span>
-            <span data-name="my-place" class="myposition c89 ft13">重庆</span>
-            <i></i>
-          </a>
+  <div id="indexbox" class="main"><!--最外层容器-->
+    <!--首页头部-->
+    <div class="indexheaer">
+      <!--头部导航-->
+      <div class="indexnavcon">
+
+        <div class="headerbox float-div">
+          <div class="main">
+            <div class="clearfix">
+              <div class="epet-search bgf">
+                <div class="fl rela ft14 location"><a href="javascript:;"><span
+                  class="catordog c89">狗狗</span> <span class="c89">|</span>
+                  <span data-name="my-place" class="myposition c89 ft13">北京</span>
+                  <i></i></a></div>
+                <p class="search-text">
+                  <a href="javascript:;">
+                  <input type="search" placeholder="搜索商品和品牌" disabled="disabled">
+                  <span class="eico serach-ico"></span>
+                  </a>
+                </p>
+                <a href="javascript:;" class="epet-category">
+                  <img src="//static.epetbar.com/static_web/wap/src/images/mydope.png">
+                </a>
+              </div>
+
+            </div>
+            <div class="find_nav">
+              <div class="find_nav_left dscroll">
+                <div class="find_nav_list dscroll-div" style="width: 140%;">
+                  <ul class="dscroll-ul">
+                    <li>
+                      <a href="javascript:;">
+                        <span class="rela"><span>首页</span><i></i></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="rela"><span>狗狗主粮</span><i></i></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="rela"><span>服饰城</span><i></i></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="rela"><span>医疗保健</span><i></i></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="rela"><span>零食玩具</span><i></i></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="rela"><span>日用外出</span><i></i></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="rela"><span>美容香波</span><i></i></span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <p class="search-text">
-          <a :href="EpetJs.Common.createUrl('search', '?pet_type='+pet_type)">
-            <input type="search" placeholder="搜索商品和品牌" disabled="">
-            <span class="eico serach-ico"></span>
-          </a>
-        </p>
-        <a :href='EpetJs.Common.createUrl("wappms/pms", "")' class="epet-category">
-          <img :src="shareurl + '/static_web/wap/src/images/mydope.png'">
-        </a>
       </div>
     </div>
+<!---->
 
+    <!--首页内容区-->
 
 
   </div>
@@ -35,7 +88,11 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  .mind
+  .main
+    display flex
+
+
+
 
 
 </style>
